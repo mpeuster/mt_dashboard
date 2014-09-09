854fb2d1fe819b375a41b22ed0e18c1296e44b65
Manuel Peuster <manuel@peuster.de>
2014-09-09 21:01:55 +0200

    Finished monitoring plots.

--- plot.js
+++ plot.js
@@ -2,7 +2,7 @@
 
 function prepend_array(a, val, size)
 {
-	for(var i = 0; i < size; i++)
+	for(var i = a.length; i < size; i++)
 	{
 		a.unshift(val);
 	}
@@ -10,87 +10,88 @@ function prepend_array(a, val, size)
 }
 
 
-function updateViewUeMonitorPlots(ue)
+function getUePlotData(ue, field, to_negative)
 {
-
-	var data = getUeHistoricalValues(ue.uri, "rx_mobile_bytes_per_second");
+	// get data
+	var data = getUeHistoricalValues(ue.uri, field);
+	// pad data
 	data = prepend_array(data, 0, MAX_HISTORY);
-
-			// Zip the generated y values with the x values
-
+	// zip
 	var res = [];
 	for (var i = 0; i < data.length; ++i) {
-			res.push([i, data[i]]);
+		if(to_negative)
+			data[i] = -data[i];
+		res.push([i, data[i]]);
 	}
+	return res;
+}
 
-	var data2 = getUeHistoricalValues(ue.uri, "tx_mobile_bytes_per_second");
-	data2 = prepend_array(data2, 0, MAX_HISTORY);
-	
-	var res2 = [];
-	for (i = 0; i < data2.length; ++i) {
-			res2.push([i, -data2[i]]); // set second bar to negative values
-	}
-
-
+function updateViewUeMonitorPlots(ue)
+{
+	// plot layout
 	var flotOptions = {
-	grid: {
-		show: true,
-		color: "#BBB",
-		borderWidth: 1,
-		borderColor: "#AAA",
-		margin: 8,
-		hoverable: false,
-		autoHighlight: false
-	},
-	 series: {
-        lines: {
-            show: true,
-            lineWidth: 1,
-            fill: true,
-            fillColor: { colors: [{ opacity: 0.7 }, { opacity: 0.1}] },
-        	},
-    	clickable: true,
-		hoverable: true,
-		shadowSize: 1
-    	},
-    colors: ["#5CE65C","#FF7070"],
-    legend: {
-	    show: true, 
-	    //show or hide legend
-	    //labelFormatter: null or (fn: string, series object -> string)
-	    //formatting your legend label by using custom functions
-	    //labelBoxBorderColor: color
-	    //label border color
-	    //noColumns: number
-	    //number of legend columns
-	    position: "sw",	    //legend position (north east, north west, south east, south west)
-	    //margin: number of pixels or [x margin, y margin]
-	    //backgroundColor: null or color
-	    //backgroundOpacity: number between 0 and 1
-	    //container: null or jQuery object/DOM element/jQuery expression        
+		grid: {
+			show: true,
+			color: "#BBB",
+			borderWidth: 1,
+			borderColor: "#AAA",
+			margin: 8,
+			hoverable: false,
+			autoHighlight: false
 		},
-	xaxis: {
-		show: false,
+		series: {
+			lines: {
+				show: true,
+				lineWidth: 1,
+				fill: true,
+				fillColor: { colors: [{ opacity: 0.7 }, { opacity: 0.1}] },
+			},
+			clickable: true,
+			hoverable: true,
+			shadowSize: 1
 		},
-	yaxis: {
-		show: true,
-		position: "right",
-	}
+		colors: ["#5CE65C","#FF7070"],
+		legend: {
+			show: true, 
+			position: "sw"
+		},
+		xaxis: {
+			show: false,
+		},
+		yaxis: {
+			show: true,
+			position: "right",
+		}
 	};
 
+	// data setup mobile
+	var plot_data_mobile = [
+	{
+		id: "rx",
+		label: "RX",
+		data: getUePlotData(ue, "rx_mobile_bytes_per_second", false)
+	},
+	{
+		id: "tx",
+		label: "TX",
+		data: getUePlotData(ue, "tx_mobile_bytes_per_second", true)
+	}
+	];
 
-	var plot_data = [
-       {
-           id: "rx",
-           label: "RX",
-           data: res
-       },
-       {
-           id: "tx",
-           label: "TX",
-           data: res2
-       }
-   ];
+	// data setup wifi
+	var plot_data_wifi = [
+	{
+		id: "rx",
+		label: "RX",
+		data: getUePlotData(ue, "rx_wifi_bytes_per_second", false)
+	},
+	{
+		id: "tx",
+		label: "TX",
+		data: getUePlotData(ue, "tx_wifi_bytes_per_second", true)
+	}
+	];
 
-	$.plot($("#plot-monitor-mobile"), plot_data, flotOptions);
+	$.plot($("#plot-monitor-mobile"), plot_data_mobile, flotOptions);
+	$.plot($("#plot-monitor-wifi"), plot_data_wifi, flotOptions);
 }
