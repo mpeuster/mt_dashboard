7b355eeadd84a01ff5d5f4c2619fc0cbd7c716d8
Manuel Peuster <manuel@peuster.de>
2014-09-16 21:17:54 +0200

    Plot outlier reuction

--- plot.js
+++ plot.js
@@ -28,11 +28,31 @@ function getUePlotData(ue, field, to_negative)
 	return res;
 }
 
+function getAvg(data)
+{
+	sum = 0;
+	for (var i = 0; i < data.length; ++i) {
+		sum += data[i][1];
+	}
+	return sum / data.length;
+}
+
 
 function updateViewUeMonitorPlots(ue)
 {
+	// receive data
+	var data_mobile_rx = getUePlotData(ue, "rx_mobile_bytes_per_second", false);
+	var data_mobile_tx = getUePlotData(ue, "tx_mobile_bytes_per_second", true);
+	var data_wifi_rx = getUePlotData(ue, "rx_wifi_bytes_per_second", false);
+	var data_wifi_tx = getUePlotData(ue, "tx_wifi_bytes_per_second", true);
+	
+	var mobile_avg = Math.max(getAvg(data_mobile_rx), -getAvg(data_mobile_tx));
+	var mobile_y = Math.max(2000, mobile_avg * 10); // use n * avg as Y scale (should help against outliers)
+	var wifi_avg = Math.max(getAvg(data_wifi_rx), -getAvg(data_wifi_tx));
+	var wifi_y = Math.max(2000, mobile_avg * 10); // use n * avg as Y scale (should help against outliers)
+
 	// plot layout
-	var flotOptions = {
+	var flotOptionsMobile = {
 		grid: {
 			show: true,
 			color: "#BBB",
@@ -64,6 +84,45 @@ function updateViewUeMonitorPlots(ue)
 		yaxis: {
 			show: true,
 			position: "right",
+			min: -mobile_y,
+			max: mobile_y,
+		}
+	};
+
+	var flotOptionsWifi = {
+		grid: {
+			show: true,
+			color: "#BBB",
+			borderWidth: 1,
+			borderColor: "#AAA",
+			margin: 8,
+			hoverable: false,
+			autoHighlight: false
+		},
+		series: {
+			lines: {
+				show: true,
+				lineWidth: 1,
+				fill: true,
+				fillColor: { colors: [{ opacity: 0.7 }, { opacity: 0.1}] },
+			},
+			clickable: true,
+			hoverable: true,
+			shadowSize: 1
+		},
+		colors: ["#5CE65C","#FF7070"],
+		legend: {
+			show: true, 
+			position: "sw"
+		},
+		xaxis: {
+			show: false,
+		},
+		yaxis: {
+			show: true,
+			position: "right",
+			min: -wifi_y,
+			max: wifi_y,
 		}
 	};
 
@@ -72,12 +131,12 @@ function updateViewUeMonitorPlots(ue)
 	{
 		id: "rx",
 		label: "RX",
-		data: getUePlotData(ue, "rx_mobile_bytes_per_second", false)
+		data: data_mobile_rx
 	},
 	{
 		id: "tx",
 		label: "TX",
-		data: getUePlotData(ue, "tx_mobile_bytes_per_second", true)
+		data: data_mobile_tx
 	}
 	];
 
@@ -86,15 +145,15 @@ function updateViewUeMonitorPlots(ue)
 	{
 		id: "rx",
 		label: "RX",
-		data: getUePlotData(ue, "rx_wifi_bytes_per_second", false)
+		data: data_wifi_rx
 	},
 	{
 		id: "tx",
 		label: "TX",
-		data: getUePlotData(ue, "tx_wifi_bytes_per_second", true)
+		data: data_wifi_tx
 	}
 	];
 
-	$.plot($("#plot-monitor-mobile"), plot_data_mobile, flotOptions);
-	$.plot($("#plot-monitor-wifi"), plot_data_wifi, flotOptions);
+	$.plot($("#plot-monitor-mobile"), plot_data_mobile, flotOptionsMobile);
+	$.plot($("#plot-monitor-wifi"), plot_data_wifi, flotOptionsWifi);
 }
